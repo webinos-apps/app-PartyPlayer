7d6ed117f7b8828ed8205c890688a838d3d6ac82
Martin Prins <martin.prins@tno.nl>
2012-11-09 18:25:16 +0100

    Add user functionality (add, remove, get), get not yet tested

--- experiments/collection/Collection.js
+++ experiments/collection/Collection.js
@@ -3,7 +3,7 @@
  * Creates a new Collection Library with the provided Name
  **/
 function Collection(name, /* optional */ randomKey){	
-	randomKey = randomKey || 10;
+	randomKey = randomKey || Math.random().toString(36).substr(2,16);
 	this.name=name;
 	this.coll={};
 	this.lastID = randomKey;
@@ -26,12 +26,12 @@ Collection.prototype.addItem = function (item,/* optional */ key){
 		}	
 	}
 	else{
-		while (this.lastID in this.coll){
-			this.lastID=this.lastID+1;
-		}		
-		id = this.lastID;
-		this.coll[this.lastID] = item;
-		return id;
+		newID="";		
+		do{		
+			newID = Math.random().toString(36).substr(2,16);
+		}while (newID in this.coll)	
+		this.coll[newID] = item;
+		return newID;
 	}	
 };
 
@@ -92,3 +92,96 @@ Collection.prototype.getItem = function(itemID){
 Collection.prototype.getItems = function (){
 	return this.coll;
 };
+
+
+
+/**
+ * Replace the item identified with the provided itemID
+ * @param itemID the item 
+ * @param item the new item corresponding for the itemID
+ * return true if item is succesfully replaced 
+ **/
+Collection.prototype.replaceItem = function(itemID, item){
+	if (this.coll.hasOwnProperty(itemID)){
+		this.coll[itemID] = item;
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+/**
+ * Updates the properties for the provided itemID
+ * @param itemID identifier 
+ * @param properties  -the properties that need to be updated. Max depth level is 2
+ * @TODO - > max two levels
+ * @TODO -> is this possible with recursiveness
+ * @returns true if the item was succesfully updated
+ * 	    false if the item does not exists of the properties have a depth > 2
+ **/
+Collection.prototype.updateItem = function (itemID, properties){
+	if (this.coll.hasOwnProperty(itemID)){
+		item = this.coll[itemID];
+	}
+	else{
+		return false;
+	}
+	if (getDepth(properties)>2){
+		return false;
+	}
+	else{
+		if (item != undefined){
+			for (key in properties){
+				if (typeof(properties[key])!='object'){
+					item[key] = properties[key];
+				}
+				if (typeof(properties[key])=='object'){
+					for (propKey in properties[key]){
+						if (typeof(properties[key][propKey])!='object'){
+							if (item[key] == undefined){
+								item[key]= {};
+							}
+							item[key][propKey] = properties[key][propKey];
+						}
+						if (typeof(properties[key][propKey])=='object'){
+							for (propPropKey in properties[key][propKey]){
+								if (typeof(properties[key][propKey][propPropKey])!='object'){
+									if (item[key][propKey] == undefined){
+										item[key][propKey] = {};
+									}
+									item[key][propKey][propPropKey] = properties[key][propKey][propPropKey];
+								}
+								if (typeof(properties[key][propKey])=='object'){
+									//not touching this
+			
+								}
+							}									
+						}
+					}	
+				}
+			}
+		}
+	}
+	this.coll[itemID] = item;
+	return true;
+}
+
+
+/**
+ * Determine depth of the object
+ **/
+function getDepth (o, depth){
+var depth = depth || 0;
+ for(var prop in o){
+    if(o.hasOwnProperty(prop)){
+      if(typeof o[prop] == 'object'){
+	depth = getDepth(o[prop], ++depth );
+      }
+    }
+  }
+  return depth;
+};
+
+
