7deeae7f78e0fc26b7eb0178861608f0d45a2fad
Martin Prins <martin.prins@tno.nl>
2012-11-27 16:06:21 +0100

    Add protocol handlers onjoin, onleave, onAdditem Updated protocol information(uml) Add user-registration on protocol level

--- apps/host/host.js
+++ apps/host/host.js
@@ -1,8 +1,68 @@
 $(document).ready(function(){
-
     partyplayer.init('host');
-
+    coll = new PartyCollection("testCollection");
 });
+
+var coll = null;
+var users = {};
+
+partyplayer.main = {};
+partyplayer.main.onjoin = function(params, ref,key) {
+    users[key]=uID; //registration on connection level.
+    uID = coll.addUser(params.name); //registration on application level
+    partyplayer.sendMessage({ns:"main", cmd:"welcome", key:key, params:{userID:uID}});
+    partyplayer.sendMessage({ns:"main", cmd:"updatePlayer", params:{userID:uID,userAlias:params.name}});
+    log('join invoked!');
+    getUsers();
+
+    //send available Items to this user
+    //items = coll.getItems();
+    //for (i=0;i<items.length;i++){
+    //	partyplayer.sendMessage({ns:"main", cmd:"
+//
+//    }
+
+};
+
+partyplayer.main.onleave= function (params, ref,key) {
+    log('leave invoked!');
+    if (typeof params == 'undefined'){ //registered on protocol level
+	if (typeof users[key] != 'undefined'){
+                userID = users[key];
+		coll.removeUser(userID);
+        	coll.removeUserItems(userID);
+		partyplayer.sendMessage({ns:"main", cmd:"removePlayer", params:{userID:userID}}); 
+	}
+    }
+    else if (typeof params != 'undefined' && params[userID] != undefined ){ //registered on application level
+	userID = params[userID];
+	coll.removeUser(userID);
+        coll.removeUserItems(userID);
+	partyplayer.sendMessage({ns:"main", cmd:"removePlayer", params:{userID:uID}}); 
+    } 
+    delete users.key; 	
+    getUsers();
+}
+
+partyplayer.main.onaddItem = function (params, ref, key) {
+    log('adding item');
+    itemID = coll.addItem(params.userID,param.item);
+    if(itemID!=false){
+	partyplayer.sendMessage({ns:"main", cmd:"updateItem", params:{userID:uID,itemID=itemID,item=param.item}}); 
+    }
+}
+
+
+function getUsers(){
+    players = coll.getUsers();
+    var str = "";
+    var nrUsers =0;
+    for (t in players){
+        nrUsers+=1;
+        str+=players[t]+",";
+    }
+    log("Currently "+nrUsers+" Users:"+str)
+}
 
     //////////// protocol implementation from here ////////////
 
@@ -13,10 +73,10 @@ $(document).ready(function(){
         participant PartyHostApp as host
         group A guest initialises
             guest -> host : join(alias)
-            host -> guest : welcome(userId)
-            note right : From now on all messages\nto the host include userId
+            host -> guest : welcome(userID)
+            note right : From now on all messages\nto the host include userID
             loop over collection
-                host -> guest : updateCollectionItem(itemId, item)
+                host -> guest : updateCollectionItem(userID,itemID, item)
             end
         end
     @enduml
@@ -29,8 +89,21 @@ $(document).ready(function(){
         participant "g:PartyGuestApp" as guest
         participant PartyHostApp as host
         group A guest shares media
-            guest -> host : shareItem(userId,_Item)
-            host -> guest : updateCollectionItem(itemId, item)
+            guest -> host : shareItem(userID,_Item)
+            host -> guest : updateCollectionItem(userID,itemID, item)
+        end
+    @enduml
+    */
+
+      /* 
+    @startuml protocol_leave.png
+        hide footbox
+        participant "g:PartyGuestApp" as guest
+        participant PartyHostApp as host
+        group A guest shares media
+            guest -> host : leave(userID)
+            host -> guest : removePlayer(userID)
+            note right: a userID may not be provided
         end
     @enduml
     */
