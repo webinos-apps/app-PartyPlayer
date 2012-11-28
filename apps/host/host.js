7e5b29d5ef2ee314c392be6d2b1671018cf0e206
Martin Prins <martin.prins@tno.nl>
2012-11-28 18:27:19 +0100

    Add funnel related message handlers. Still need to be tested

--- apps/host/host.js
+++ apps/host/host.js
@@ -3,37 +3,37 @@ var coll = null;
 var users = {};
 
 partyplayer.main = {};
+partyplayer.funnel = {};
+
 partyplayer.main.onjoin = function(params, ref, key) {
     uID = coll.addUser(params); //registration on application level
     users[key]=uID; //registration on connection level.
     partyplayer.sendMessage({ns:"main", cmd:"welcome", params:{userID:uID}}, key);
-    
     pUsers = coll.getUsers();
     for (var u in pUsers){
-         partyplayer.sendMessage({ns:"main", cmd:"updatePlayer", params:{userID:uID,user:pUsers[u]}}, key);      
+        if(uID != u){
+            partyplayer.sendMessage({ns:"main", cmd:"updatePlayer", params:{userID:uID,user:pUsers[u]}}, key);      
+        }
     }    
-   
     partyplayer.sendMessage({ns:"main", cmd:"updatePlayer", params:{userID:uID,user:coll.getUser(uID)}});
-    log('join invoked!');
+    //log('join invoked!');
     getUsers();
-
     //send available Items to this user
     pItems = coll.getItems();
     for (var i=0; i<pItems.length;i++){
         partyplayer.sendMessage({ns:"main", cmd:"updateCollectionItem", params:pItems[i]})
     }
-
 };
 
 partyplayer.main.onleave= function (params, ref, key) {
-    log('leave invoked!');
+    //log('leave invoked!');
     if (typeof params === 'undefined'){ //registered on protocol level
-    if (typeof users[key] !== 'undefined'){
-        userID = users[key];
-        coll.removeUser(userID);
-        coll.removeUserItems(userID);
-        partyplayer.sendMessage({ns:"main", cmd:"removePlayer", params:{userID:userID}}); 
-    }
+        if (typeof users[key] !== 'undefined'){
+            userID = users[key];
+            coll.removeUser(userID);
+            coll.removeUserItems(userID);
+            partyplayer.sendMessage({ns:"main", cmd:"removePlayer", params:{userID:userID}}); 
+        }
     }
     else if (typeof params !== 'undefined' && params[userID] !== undefined ){ //registered on application level
         userID = params[userID];
@@ -50,10 +50,24 @@ partyplayer.main.onaddItem = function (params, ref, key) {
     log('adding item');
     itemID = coll.addItem(params.userID,params.item);
     if(itemID!==false){
-    partyplayer.sendMessage({ns:"main", cmd:"updateCollectionItem", params:{userID:uID,itemID:itemID,item:params.item}}); 
+        partyplayer.sendMessage({ns:"main", cmd:"updateCollectionItem", params:{userID:uID,itemID:itemID,item:params.item}}); 
     }
     getItems();
 };
+
+
+partyplayer.funnel.onaddItem = function( params,ref, key) {
+    log("got a new item for the funnel");   
+    funnelItemID = funnel.addItem(itemID);
+    partyplayer.sendMessage({"ns":"funnel",cmd:"updateFunnelItem", params:{userID:uID,funnelItemID:funnelItemID,vote:voteresult}});
+}
+
+partyplayer.funnel.onvote = function (params, ref, key) {
+    log("got a vote");
+    funnel.vote(userID,itemID,funnelItemID,vote);
+    //user "pietje" upvoted item in funnelID "blaat"
+    partyplayer.sendMessage({ns:"funnel", cmd:"updateFunnelItem", params:{userID:uID,funnelItemID:funnelItemID,vote:voteresult}});
+}
 
 
 function getUsers(){
